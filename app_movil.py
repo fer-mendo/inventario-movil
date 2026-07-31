b0782afde532b5effef06a52e94201f75c60dbee
fer-mendo <f.monneretscg@gmail.com>
2026-07-31 15:59:13 -0300

    Fix logo file extension in app_movil.py

--- app_movil.py
+++ app_movil.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="MendoMedica - Inventario", page_icon="🏥", layo
 # ==============================================================================
 # 🖼️ ARCHIVO DEL LOGO DE LA EMPRESA
 # ==============================================================================
-URL_LOGO = "logo.png.jpeg"
+URL_LOGO = "logo.png"
 
 # ==============================================================================
 # 🏬 LISTA MAESTRA DE ALMACENES
